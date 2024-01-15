c973a2740ad60e07aebb3821e36bcb27251ff74a
Junxiong Zhang <1610580113@qq.com>
2024-01-15 10:41:58 +0800

    add: text process

--- DL0/utils.py
+++ DL0/utils.py
@@ -101,3 +101,46 @@ def accuracy(y, y_hat):
     res = (y.data == pred)
     acc = res.mean()
     return DL0.Variable(np.array(acc))
+
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .').replace(',', ' ,').replace('!', ' !')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+
+def create_contexts_target(corpus, window_size=1):
+    """生成上下文和目标词
+
+        :param corpus: 语料库（单词ID列表）
+        :param window_size: 窗口大小（当窗口大小为1时，左右各1个单词为上下文）
+        :return:
+    """
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
+
+def convert_to_onehot(x, vocab_size):
+    return np.eye(vocab_size)[x.data]
